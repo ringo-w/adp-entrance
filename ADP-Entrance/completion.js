ec6f89ef8a3c4d5580e1818fecd4e5e1675341ba
ringo-w <36806106+ringo-w@users.noreply.github.com>
2020-01-05 00:20:00 -0800

    removed comments, console.logs, and extra code

--- ADP-Entrance/completion.js
+++ ADP-Entrance/completion.js
@@ -1,9 +1,6 @@
 const finalScore = document.getElementById("finalScore");
 const currentScore = localStorage.getItem("currentScore");
 
-// change pass/fail message based on current score (e.g. pass if 2 or more answers are correct)
 document.getElementById("passOrFail").innerHTML = currentScore >= 2 ? "You Passed!" : "You Failed!";
-// change the pass/fail message to red or green depending on incorrect or correct answer respectively
 document.getElementById("passOrFail").style.color = currentScore >= 2 ? "green" : "red";
-// show current score
 finalScore.innerText = "Score: " + currentScore + "/3";
--- ADP-Entrance/quiz.js
+++ ADP-Entrance/quiz.js
@@ -3,7 +3,6 @@ const choices = Array.from(document.getElementsByClassName("choice-text"));
 const scoreText = document.getElementById("score");
 const quiz = document.getElementById("quiz");
 
-// retrieve quiz number 
 let qNum = localStorage.getItem("quizChoice"); 
 let score = 0;
 let answerKey = 0; 
@@ -14,32 +13,25 @@ let qRemaining = [];
 let answerValue = [];
 let acceptingAnswers = false;
 
-// set quiz and launch quiz
 function quizNum(elem) {
   qNum = elem.getAttribute("id");
   localStorage.setItem("quizChoice", qNum); 
   window.location.href="quiz.html";
 }
-// get data from json file
+
 fetch("quiz.json")
 .then(response => {
   return response.json();
 })
 .then(data => {
-  // put all question data together
   questions = data.quizzes[qNum].questions.map(questionData => {
     const eachQuestion = {
     question: questionData.question
     }; 
-    console.log(eachQuestion);
-    
-    // put all answer choices together
     const answerChoices = questionData.answers;
     answerChoices.forEach((choice, index) => {
       eachQuestion["choice" + (index + 1)] = choice;
     }); 
-    console.log(answerChoices);
-    
     return eachQuestion;
   });
   startQuiz();
@@ -47,7 +39,7 @@ fetch("quiz.json")
 .catch(error => {
   console.error('Error:', error);
 });
-// function to start quiz - will reset counters, load questions, get new questions
+
 startQuiz = () => {
   questionCounter = 0;
   score = 0;
@@ -57,35 +49,28 @@ startQuiz = () => {
 };
 
 getNewQuestion = () => {
-  // check if questions are done, if so, go to completion page
   if (qRemaining.length === 0 || questionCounter >= 3) {
     localStorage.setItem("currentScore", score);
     return window.location.assign("completion.html");
   }
-
   questionCounter++;
   answerValue = [];
-  // set random question order
   const questionIndex = Math.floor(Math.random() * qRemaining.length);
   currentQ = qRemaining[questionIndex];
   question.innerHTML = currentQ.question;
   choices.forEach(choice => {
     const number = choice.dataset["number"];
     const answerTitle = currentQ["choice" + number].content;
-    console.log(answerTitle);
-    
     choice.innerHTML = answerTitle;
-    // array of boolean values 
     answerValue.push(currentQ["choice" + number].value);
   });
-  // find which index contains the 'true' boolean
+
   for (let i = 0; i < answerValue.length; i++) {
     if (answerValue[i]) {
       answerKey = i+1;
       break;
     }
   }
-  // remove previous question
   qRemaining.splice(questionIndex, 1);
   acceptingAnswers = true;
 };
@@ -97,14 +82,10 @@ choices.forEach(choice => {
     const selectedChoice = e.target;
     const selectedAnswer = selectedChoice.dataset["number"];
     const classToApply =
-    // check if answer key value matches index of value: true
     selectedAnswer == answerKey ? "correct" : "incorrect"; 
-    console.log(answerKey);
-    
     if (classToApply === "correct") {
       incrementScore(1);
     }
-    // set 2 second delay to move onto next question
     selectedChoice.parentElement.classList.add(classToApply);
     setTimeout(() => {
       selectedChoice.parentElement.classList.remove(classToApply);
